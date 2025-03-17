5da6d224b8365b4a309f89b00a3a5604ac8073a1
lazalajochy <jrosario@intellisysdcorp.com>
2025-03-17 15:14:54 -0400

    test again

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
-console.log("test-running")
+console.log("this is not working...")
 app.use(express.json());
 app.use(passport.initialize());
 passport.use(passportMiddlewares)
